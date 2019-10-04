8776f0afd0dd858cd12db666a30e5a15a7ce19c7
Bekarys Kuspan <kusbek1994@gmail.com>
2019-10-04 21:33:47 +0600

    Admin objects for blogs ahve meaningfull titles, summary is cut, we display pretty date instead of date + time

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,13 @@ class Blog(models.Model):
     body = models.TextField(max_length = 10000)
     image = models.ImageField(upload_to = 'images/')
     pub_date = models.DateTimeField()
+
+
+    def __str__(self):
+        return self.title
+        
+    def summary(self):
+        return self.body[:165]
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
